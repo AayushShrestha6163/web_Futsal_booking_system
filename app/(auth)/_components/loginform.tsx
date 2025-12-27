fec4f14a9cd0082c055cd88c4d6253665aa13561
AayushShrestha6163 <aayushshrestha6163@gmail.com>
2025-12-27 14:50:00 +0545

    loginform done

--- app/(auth)/_components/loginform.tsx
+++ app/(auth)/_components/loginform.tsx
@@ -16,7 +16,7 @@ export default function LoginForm() {
     formState: { errors, isSubmitting },
   } = useForm<LoginData>({
     resolver: zodResolver(loginSchema),
-    mode: "onSubmit",
+    mode: "onSubmi",
   });
 
   const submit = async (values: LoginData) => {
